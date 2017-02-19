25b8ea105ad5452de5f70f729fd0edeb47f291f1
ChrisInSeattle <chris@thecatball.com>
2017-02-18 18:37:51 -0800

    Code to parse datetime ranges sent from alexa

---
+++ AlexaSkillsKit.Lib/DateRangeParser/AlexaDateRangeConvertException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AlexaSkillsKit.DateRangeParser
+{
+    [Serializable]
+    public class AlexaDateRangeConvertException : Exception
+    {
+        public AlexaDateRangeConvertException()
+        {
+        }
+
+        public AlexaDateRangeConvertException(string message) : base(message)
+        {
+        }
+
+        public AlexaDateRangeConvertException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AlexaDateRangeConvertException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
