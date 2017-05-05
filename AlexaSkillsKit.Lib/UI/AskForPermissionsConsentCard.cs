2c43296ef286749d3d31234ba7869207992c9cc6
Jernigan, Eric <eric.jernigan@egov.com>
2017-05-05 10:56:26 -0500

    adding the json structure for getting echo device id.  Also creating the permissions card for requesting the correct scope permission (full address or country zip)

---
+++ AlexaSkillsKit.Lib/UI/AskForPermissionsConsentCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexaSkillsKit.UI
+{
+    public class AskForPermissionsConsentCard : Card
+    {
+        public override string Type
+        {
+            get { return "AskForPermissionsConsent"; }
+        }
+
+        protected virtual string[] Permissions { get; private set; }
+
+        public virtual PermissionType PermissionType {
+
+            set
+            {
+                Permissions = value == PermissionType.FullAddress
+                    ? new string[] {"read::alexa:device:all:address"}
+                    : new string[] {"read::alexa:device:all:address:country_and_postal_code"};
+            }
+        }
+
+        
+    }
+
+    public enum  PermissionType
+    {
+        FullAddress,
+        CountryAndPostalCode
+    }
+}
