929bf05bb28e3a5ced01c48e7d8916203983793c
Jayson Helseth <jayson.helseth@med.und.edu>
2017-07-11 14:34:40 -0500

    Added a fix for the parsing of the timestamp from the Amazon request. Sometimes the timestamp can vary.

---
+++ AlexaSkillsKit.Lib/Helpers/DateTimeUtils.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace AlexaSkillsKit.Helpers
+{
+    public class DateTimeUtils
+    {
+        public static DateTime FromAmazonRequest(string datetimeString)
+        {
+            try
+            {
+                DateTime datetime;
+
+                //Try a generic parse
+                if (DateTime.TryParse(datetimeString, out datetime))
+                {
+                    return datetime;
+                }
+
+                //Try a ticks parse
+                long ticks;
+                long.TryParse(datetimeString, out ticks);
+                if (ticks > 0)
+                {
+                    var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+                    datetime = epoch.AddMilliseconds(ticks);
+                    return datetime;
+                }
+
+                //Try an offset parse for the Iso format
+                var offset = DateTimeOffset.Parse(datetimeString);
+                return offset.DateTime;
+            }
+            catch (Exception e)
+            {
+                return DateTime.Now;
+            }
+        }
+    }
+}
