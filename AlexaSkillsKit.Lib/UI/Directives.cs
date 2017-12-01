055591f28654f0adbbcf0527cbd31406ab4cdb29
Sergey Greenko <ElvenMonk@mail.ru>
2017-12-01 23:19:49 +0200

    Reverted unrelated package version changes

--- AlexaSkillsKit.Lib/UI/Directives.cs
+++
@@ -1,8 +0,0 @@
-﻿//  Copyright 2015 Stefan Negritoiu (FreeBusy). See LICENSE file for more information.
-
-public class Directive
-{
-    public virtual string PlayBehavior { get; set; } //TODO: Enum?
-    public virtual AudioItem AudioItem { get; set; }
-    public string Type { get; set; } //TODO: Enum?
-}
--- AlexaSkillsKit.Lib/UI/Stream.cs
+++
@@ -1,9 +0,0 @@
-﻿//  Copyright 2015 Stefan Negritoiu (FreeBusy). See LICENSE file for more information.
-
-
-public class AudioStream
-{
-    public string Token { get; set; }
-    public string Url { get; set; }
-    public long OffsetInMilliseconds { get; set; }
-}
